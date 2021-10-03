6e05caae8215f172d816d92f0f8d965aeb79f066
Mona <moniquenagel24.mn@gmail.com>
2021-10-03 15:01:06 +0200

    Added get, set, to string, contructor, hash, etc. methods to entities: AccountType and AccountTransaction

--- AccountSystemDomain/src/main/java/za/ac/nwu/das/domain/persistence/AccountTransaction.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/das/domain/persistence/AccountTransaction.java
@@ -1,32 +1,107 @@
 package za.ac.nwu.das.domain.persistence;
 
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Table(name = "Account_Transaction", schema = "dbo")
+@Table(name = "ACCOUNT_TRANSACTION", schema = "")
 public class AccountTransaction implements Serializable {
 
-    @Id
-    @SequenceGenerator(name = "DBO_GENERIC_SEQ",
-            sequenceName = "dbo.DBO_GENERIC_SEQ",
-            allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-                    generator = "DBO_GENERIC_SEQ")
-    @Column(name = "transaction_id")
+    private static final long serialVersionUID = 6926049907460965566L;
+
     private Long transactionId;
-
-    @Column(name = "account_type_id")
-    private Long accountTypeId;
-
-    @Column(name = "member_id")
+    private AccountType accountType;
     private Long memberId;
-
-    @Column(name = "transaction_amount")
     private Long transactionAmt;
-
-    @Column(name = "transaction_date")
     private LocalDate transactionDate;
 
+
+    public AccountTransaction() {
+    }
+
+    public AccountTransaction(Long transactionId, AccountType accountType, Long memberId, Long transactionAmt, LocalDate transactionDate) {
+        this.transactionId = transactionId;
+        this.accountType = accountType;
+        this.memberId = memberId;
+        this.transactionAmt = transactionAmt;
+        this.transactionDate = transactionDate;
+    }
+
+    @Id
+    @SequenceGenerator(name = "DBO_GENERIC_SEQ",sequenceName = "dbo.DBO_GENERIC_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "DBO_GENERIC_SEQ")
+    @Column(name = "TRANSACTION_ID")
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_TYPE_ID")
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    @Column(name = "MEMBER_ID")
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    @Column(name = "TRANSACTION_AMOUNT")
+    public Long getTransactionAmt() {
+        return transactionAmt;
+    }
+
+    @Column(name = "TRANSACTION_DATE")
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setTransactionAmt(Long transactionAmt) {
+        this.transactionAmt = transactionAmt;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountTransaction that = (AccountTransaction) o;
+        return Objects.equals(transactionId, that.transactionId) && Objects.equals(accountType, that.accountType) && Objects.equals(memberId, that.memberId) && Objects.equals(transactionAmt, that.transactionAmt) && Objects.equals(transactionDate, that.transactionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, accountType, memberId, transactionAmt, transactionDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountTransaction{" +
+                "transactionId=" + transactionId +
+                ", accountType=" + accountType +
+                ", memberId=" + memberId +
+                ", transactionAmt=" + transactionAmt +
+                ", transactionDate=" + transactionDate +
+                '}';
+    }
 }
--- AccountSystemDomain/src/main/java/za/ac/nwu/das/domain/persistence/AccountType.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/das/domain/persistence/AccountType.java
@@ -3,34 +3,90 @@ package za.ac.nwu.das.domain.persistence;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "Account_Transaction", schema = "dbo")
 public class AccountType implements Serializable {
 
-    private static final long serialVersionUID = -2845191564523985042L;
+    private static final long serialVersionUID = 2566762841659265490L;
+
+    private Long accountTypeId;
+    private Long mnemonic;
+    private String accountTypeName;
+
+    private Set<AccountTransaction> accountTransactions;
+
+    public AccountType() {
+    }
+
+    public AccountType(Long accountTypeId, Long mnemonic, String accountTypeName) {
+        this.accountTypeId = accountTypeId;
+        this.mnemonic = mnemonic;
+        this.accountTypeName = accountTypeName;
+
+    }
 
     @Id
-    @SequenceGenerator(name = "DBO_GENERIC_SEQ",
-                       sequenceName = "dbo.DBO_GENERIC_SEQ",
-                       allocationSize = 1)
+    @SequenceGenerator(name = "DAS_GENERIC_SEQ",
+                sequenceName = "dbo.DAS_GENERIC_SEQ",
+                allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
-                    generator = "DBO_GENERIC_SEQ")
+            generator = "DAS_GENERIC_SEQ")
     @Column(name = "account_type_id")
-    private Long accountTypeId;
+    public Long getAccountTypeId() {
+        return accountTypeId;
+    }
+
 
     @Column(name = "mnemonic")
-    private Long mnemonic;
+    public Long getMnemonic() {
+        return mnemonic;
+    }
 
     @Column(name = "account_type_name")
-    private Long accountTypeName;
+    public String getAccountTypeName() {
+        return accountTypeName;
+    }
 
+    @OneToMany(targetEntity = AccountTransaction.class,
+               fetch = FetchType.LAZY,
+               mappedBy = "AccountType",
+               orphanRemoval = true,
+               cascade = CascadeType.PERSIST)
+    public Set<AccountTransaction> getAccountTransactions(){
+        return accountTransactions;
+    }
 
+    public void AccountTransactions(Set<AccountTransaction> accountTransactions) {
+        this.accountTransactions = accountTransactions;
+    }
 
+    public void setAccountTypeId(Long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
 
+    public void setMnemonic(Long mnemonic) {
+        this.mnemonic = mnemonic;
+    }
 
+    public void setAccountTypeName(String accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountType that = (AccountType) o;
+        return Objects.equals(accountTypeId, that.accountTypeId) && Objects.equals(mnemonic, that.mnemonic) && Objects.equals(accountTypeName, that.accountTypeName);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountTypeId, mnemonic, accountTypeName);
+    }
 
     @Override
     public String toString() {
